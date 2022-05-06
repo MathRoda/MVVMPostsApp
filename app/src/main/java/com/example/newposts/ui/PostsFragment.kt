1bbdd68afb11f0f387fb7049e63d6e522d64873c
MathRoda <mouawiahammo65@gmail.com>
2022-05-06 21:01:28 +0200

    v 0.1 edits

--- app/src/main/java/com/example/newposts/ui/PostsFragment.kt
+++ app/src/main/java/com/example/newposts/ui/PostsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
 import androidx.fragment.app.activityViewModels
 import com.example.newposts.databinding.PostsFragmentBinding
 
@@ -25,6 +26,7 @@ class PostsFragment : Fragment() {
         binding.lifecycleOwner =this
         binding.viewModel = viewModel
         binding.rvPosts.adapter = PostsAdapter()
+
 
 
         return binding.root
